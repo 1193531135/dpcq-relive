b975e7b6edb5f3acabee2cbddb4217109e175be4
single <j1193531135@gmail.com>
2023-09-07 18:41:08 +0800

    some

---
+++ catchImage.py
@@ -0,0 +1,59 @@
+import math
+from os import system
+from itertools import repeat
+import threading
+import win32gui
+import win32api
+import win32con
+import time
+import json
+import re
+import mss
+
+def capture_process_screenshot(MainProcessId, output_file):
+    window = win32gui.GetWindowRect(MainProcessId)
+    left, top, right, bottom = window
+    if window:
+        region = {
+          "left": left,
+          "top": top,
+          "width": right - left,
+          "height": bottom - top
+        }
+        with mss.mss() as sct:
+            region = window
+            sct.shot(output=output_file)
+            # cropped_screenshot = sct.crop((left, top, right, bottom))
+            # cropped_screenshot.save(output_file)
+            print(f"截图已保存到 {output_file}")
+
+def getProcessName(jubing, mouse,array,selectName):
+    if(win32gui.IsWindow(jubing) and win32gui.IsWindowVisible(jubing)):
+        name = win32gui.GetWindowText(jubing)
+        if name.find(selectName) != -1:
+            array.append({
+                'name': name,
+                'processId': jubing,
+            })
+def findMainProcess():
+    system('cls')
+    print('开始检索主进程...')
+    array = []
+    MainProcessId = 0
+    name = ""
+    win32gui.EnumWindows(lambda j,m:getProcessName(j,m,array,name), 0)
+    if len(array) == 0:
+        input('未找到目标进程（按任意键返回菜单）')
+        return getBrowser()
+    # 只有一个，直接进入控制
+    if len(array) == 1:
+        print()
+        MainProcessId = array[0]['processId']
+    if len(array) > 1:
+        for index,item in enumerate(array):
+            print(f"{index}. {item['name']}")
+        MainProcessId = array[int(input(f"""查询到 {len(array)} 条进程， 选择id，按下回车键（enter）:"""))]['processId']
+        capture_process_screenshot(MainProcessId,"catchImage.png")
+
+
+findMainProcess()
--- getImage.py
+++ getImage.py
@@ -23,57 +23,67 @@ def window_capture(filename,hwnd):
     w = right - left
     h = bottom - top
     # print w,h　　　#图片大小
-    image = { 'w':100,'h':100 }
-    # 截取从左上角（0，0）长宽为（w，h）的图片
-    startX = int(w/2+132)
-    starty = int(h/2-230)
+    image = { 'w':200,'h':200 }
+    # 截取从左上角（startX，starty）长宽为（w，h）的图片
+    startX = int(0)
+    starty = int(0)
     # 为bitmap开辟空间
-    saveBitMap.CreateCompatibleBitmap(mfcDC,image['w'], image['h'])
+    saveBitMap.CreateCompatibleBitmap(mfcDC,w, h)
     # 高度saveDC，将截图保存到saveBitmap中
     saveDC.SelectObject(saveBitMap)
-    saveDC.BitBlt((0,0), (image['w'], image['h']), mfcDC, (startX, starty), win32con.SRCCOPY)
+    saveDC.BitBlt((0,0), (w, h), mfcDC, (startX, starty), win32con.SRCCOPY)
     # saveDC.BitBlt(((w-image['w'])/2,(h-image['h'])/2), (image['w'], image['h']), mfcDC, (0, 0), win32con.SRCCOPY)
     saveBitMap.SaveBitmapFile(saveDC, filename)
 
 # window_capture("haha.jpg")
 array = []
-childrenArray = []
 # name = ''
-name = '175游戏浏览器'
+name = ''
 def getChildren(jubing, mouse,childrenS,chidrenNeed):
-    # chidrenNeed为false检索qq浏览器子进程text名 =>  '无标题 - QQ浏览器'
     if(win32gui.IsWindow(jubing) and win32gui.IsWindowVisible(jubing)):
-        name = win32gui.GetClassName(jubing) if chidrenNeed else win32gui.GetWindowText(jubing)
+        name = win32gui.GetWindowText(jubing)
+        className = win32gui.GetClassName(jubing)
         # name = win32gui.GetWindowText(jubing)
         # print(name)
         # MacromediaFlashPlayerActiveX 为ie下的flash适配名称
-        if(name.find('MacromediaFlashPlayerActiveX') != -1 and chidrenNeed):
+        name = name if name else className
+        if(True):
+        # if(name.find('MacromediaFlashPlayerActiveX') != -1 and chidrenNeed):
             childrenS.append({
-                'name': name or 'null',
+                'name': name,
                 'processId': jubing,
+                'className': className,
             })
 
 def getProcessName(jubing, mouse,array,selectName):
     if(win32gui.IsWindow(jubing) and win32gui.IsWindowVisible(jubing)):
         name = win32gui.GetWindowText(jubing)
+        className = win32gui.GetClassName(jubing)
+        name = name if name else className
         if name.find(selectName) != -1:
             array.append({
-                'name': name,
+                'name': name or className,
                 'processId': jubing,
+                'className': className,
             })
 win32gui.EnumWindows(lambda j,m:getProcessName(j,m,array,name), 0)
 # print(array)
-win32gui.EnumChildWindows(array[0]['processId'],lambda a,b:getChildren(a,b,childrenArray,True),0)
-print(childrenArray)
-hwnd = childrenArray[0]['processId']
+for index,item in enumerate(array):
+  childrenArray = []
+  win32gui.EnumChildWindows(item['processId'],lambda a,b:getChildren(a,b,childrenArray,True),0)
+  print(f"{index}. {item['name']}----{item['processId']}")
+  if len(childrenArray) > 0:
+    item['children'] = childrenArray
+    for index2,item2 in enumerate(childrenArray):
+      print(f"----{index2}. {item2['name']}----{item2['processId']}")
+
+# hwnd = childrenArray[0]['processId']
 # 窗口先设置为活跃再截图，父窗口才行
 # win32gui.SetForegroundWindow(array[0]['processId'])
+ids = input(f"""查询到 {len(array)} 条进程， 选择id，按下回车键（enter）:""").split(',')
+hwnd = array[int(ids[0])]['processId'] if len(ids) == 1 else array[int(ids[0])]['children'][int(ids[1])]['processId']
+print(hwnd)
+startTime = time.time() * 1000
 window_capture("position.jpg",hwnd)
-# ocr = PaddleOCR(use_angle_cls = True,lang="en")
-# text=ocr.ocr("./position.jpg",cls=True)
-# print(text)
-# beg = time.time()
-# for i in range(10):
-#     window_capture("haha.jpg")
-# end = time.time()
-# print(end - beg)
+endTime = time.time() * 1000
+print(endTime - startTime)
